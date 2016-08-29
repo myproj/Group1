633f0c11874fbb1ca0cbdc72e045d9ee747670da
sunil kumar <sunil_k@students.iitmandi.ac.in>
2016-08-29 16:29:34 +0530

    sunil fac_change

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,11 @@
 #include "functions.h"
-
+#include<stdio.h>
 int factorial(int n){
+	if(n<=0)
+	{
+		printf("Please Re-enter +ve number");
+		return -1;
+	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
