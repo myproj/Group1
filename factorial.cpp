575bd798fa1f68d16afaba3861c038b202b7b45e
myproj <cs308lsap@gmail.com>
2016-08-29 16:13:16 +0530

    initial project version

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,10 @@
 #include "functions.h"
+#include <cstdio>
 
 int factorial(int n){
-    if(n!=1){
+    if(n<0)
+	printf("Re-enter a positive number\n");
+    else if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
