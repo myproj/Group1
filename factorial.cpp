d0857ee82f7490b25994a92d300b62754b72b7b1
Parul <parulever333@gmail.com>
2016-09-05 16:36:39 +0530

    Reverting Back in master

--- factorial.cpp
+++ factorial.cpp
@@ -3,11 +3,6 @@
 #include "functions.h"
 #include<stdio.h>
 int factorial(int n){
-	if(n<=0)
-	{
-		cout<<"Please Re-enter +ve number";
-		return -1;
-	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
