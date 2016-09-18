42a5c2a16b813eacdc1436153d319e6cf5c10eae
Parul <parulever333@gmail.com>
2016-09-18 20:38:52 +0530

    Changes in Add_Digit

--- Add_Digit.cpp
+++ Add_Digit.cpp
@@ -1,23 +1,30 @@
+/* Group 1
+ It prints out the sum of digits in number by calculating remainder
+*/
+
+
 #include<stdio.h>
+#include<iostream>
+using namespace std;
  
 int main()
 {
    int n, sum = 0, remainder;
  
-   printf("Enter an integer\n");
-   scanf("%d",&n);
+   cout<<"Enter an integer\n";
+   cin>>n;
     
    while(n != 0)
    {
       remainder = n % 10;
-	  printf("The Remainder is %d\n", remainder);
+	  cout<<"The Remainder is "<<remainder<<"\n";
       sum = sum + remainder;
-	  printf("The Sum is %d\n", sum);
+	  cout<<"The Sum is "<<sum<<"\n";
       n = n / 10;
-	  printf("The n is %d\n", n);
+	  cout<<"The n is "<<n<<"\n";
    }
  
-   printf("Sum of digits of entered number = %d\n",sum);
+   cout<<"Sum of digits of entered number = "<<sum<<"\n";
  
    return 0;
 }
